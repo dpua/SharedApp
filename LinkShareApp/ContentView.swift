453a479583976266a23c00ab907f4cc050697b57
Ian <dp.ua@mail.com>
2026-03-14 21:21:21 +0100

    create project

---
+++ LinkShareApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkShareApp
+//
+//  Created by ian on 14.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkShareApp/LinkShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkShareAppApp.swift
+//  LinkShareApp
+//
+//  Created by ian on 14.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LinkShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
